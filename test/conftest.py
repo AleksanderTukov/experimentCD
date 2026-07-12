9dcb38214ac0d7950378dca28640ee8de17798da
AleksanderTukov <129859015+AleksanderTukov@users.noreply.github.com>
2026-07-12 22:45:39 +0300

    Delete test/conftest.py

--- test/conftest.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-
-# Корень проекта — это папка выше, чем test/
-root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Добавляем root_dir в sys.path, чтобы `from src.main import ...` работал
-sys.path.insert(0, root_dir)
